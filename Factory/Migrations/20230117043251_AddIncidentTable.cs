af6c1185d428a862600ce6d1f1b902a6b11dc9a1
delguanzon <yodelguanzon@gmail.com>
2023-01-16 20:33:19 -0800

    Add migration for IncidentTable and new date props

---
+++ Factory/Migrations/20230117043251_AddIncidentTable.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectName.Migrations
+{
+    public partial class AddIncidentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Engineers_Locations_LocationId",
+                table: "Engineers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_Locations_LocationId",
+                table: "Machines");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations");
+
+            migrationBuilder.RenameTable(
+                name: "Locations",
+                newName: "Location");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "LastServiced",
+                table: "Machines",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "NextService",
+                table: "Machines",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "LicenseExpiry",
+                table: "Engineers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Location",
+                table: "Location",
+                column: "LocationId");
+
+            migrationBuilder.CreateTable(
+                name: "Incident",
+                columns: table => new
+                {
+                    IncidentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsResolved = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    EngineerId = table.Column<int>(type: "int", nullable: false),
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incident", x => x.IncidentId);
+                    table.ForeignKey(
+                        name: "FK_Incident_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Incident_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Incident_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incident_EngineerId",
+                table: "Incident",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incident_LocationId",
+                table: "Incident",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incident_MachineId",
+                table: "Incident",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Engineers_Location_LocationId",
+                table: "Engineers",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_Location_LocationId",
+                table: "Machines",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Engineers_Location_LocationId",
+                table: "Engineers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_Location_LocationId",
+                table: "Machines");
+
+            migrationBuilder.DropTable(
+                name: "Incident");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Location",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "LastServiced",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "NextService",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "LicenseExpiry",
+                table: "Engineers");
+
+            migrationBuilder.RenameTable(
+                name: "Location",
+                newName: "Locations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Engineers_Locations_LocationId",
+                table: "Engineers",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_Locations_LocationId",
+                table: "Machines",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
